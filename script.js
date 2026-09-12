989b043588d31db8444190c8175f5c2b17ebe06a
becausecannotbeblank <resubuhtig@proton.me>
2026-09-12 14:58:34 +0200

    Console version finished.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function gameBoard(){
 
     const makeMark = (row, column, player) => {
         const availableCell = board[row][column];
-        console.log(availableCell.getValue());
+     //   console.log(availableCell.getValue());
         if(availableCell.getValue() !==0){
             console.log("Cell taken.")
             return;
@@ -24,13 +24,80 @@ function gameBoard(){
     }
 
     const printBoard = () => {
-        const boardWithCells = board.map((row) =>     // Replace printBoard checks or add to it with winchecks.
+        const boardWithCells = board.map((row) =>
             row.map((cell) => cell.getValue())
         );
         console.log(boardWithCells);
     }
 
-    return {getBoard, makeMark, printBoard};
+    //if arrays horizontally/diagonally/vertically same, then win - use looping since every row is an array and each column is an array within!
+
+    const checkWin = () => {
+        const topRow = board[0];
+        const midRow = board[1];
+        const botRow = board[2];
+
+        const leftDia = [board[0][0], board[1][1], board[2][2]];
+        const rightDia = [board[0][2], board[1][1], board[2][0]];
+
+        const leftCol = [board[0][0], board[1][0], board[2][0]];
+        const midCol = [board[0][1], board[1][1], board [2][1]];
+        const rightCol = [board[0][2], board[1][2], board [2][2]];
+
+        if(topRow.every(cell => cell.getValue() === "x")){
+            console.log("x wins upper row");
+        }
+        if(topRow.every(cell => cell.getValue() === "o")){
+            console.log("o wins upper row");
+        }
+        if(midRow.every(cell => cell.getValue() === "x")){
+            console.log("x wins middle row");
+        }
+        if(midRow.every(cell => cell.getValue() === "o")){
+            console.log("o wins middle row");
+        }
+        if(botRow.every(cell => cell.getValue() === "x")){
+            console.log("x wins bottom row");
+        }
+        if(botRow.every(cell => cell.getValue() === "o")){
+            console.log("o wins bottom row");
+        }
+
+        if(leftDia.every(cell => cell.getValue() === "o")){
+            console.log("o wins left diagonal");
+        }
+        if(leftDia.every(cell => cell.getValue() === "x")){
+            console.log("x wins left diagonal");
+        }
+        if(rightDia.every(cell => cell.getValue() === "o")){
+            console.log("o wins right diagonal");
+        }
+        if(rightDia.every(cell => cell.getValue() === "x")){
+            console.log("x wins right diagonal");
+        }
+
+        if(leftCol.every(cell => cell.getValue() === "x")){
+            console.log("x wins left column");
+        }
+        if(leftCol.every(cell => cell.getValue() === "o")){
+            console.log("o wins left column");
+        }
+        if(midCol.every(cell => cell.getValue() === "x")){
+            console.log("x wins middle column");
+        }
+        if(midCol.every(cell => cell.getValue() === "o")){
+            console.log("o wins middle column");
+        }
+        if(rightCol.every(cell => cell.getValue() === "x")){
+            console.log("x wins right column");
+        }
+        if(rightCol.every(cell => cell.getValue() === "o")){
+            console.log("o wins right column");
+        }
+    }
+
+
+    return {getBoard, makeMark, printBoard, checkWin};
 }
 
 function Cell(){
@@ -82,6 +149,8 @@ function Controller(
             `${getActivePlayer().name} marks cell: ${row}, ${column}.`
         )
         board.makeMark(row, column, getActivePlayer().mark);
+
+        board.checkWin();
 
         switchTurn();
         printBoard();
